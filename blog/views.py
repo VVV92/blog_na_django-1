13d5196587b35d4dc44ec9004690f326b33501a4
VVV92 <UPGWolf@gmail.com>
2024-08-12 13:40:48 +0300

    Добавление постраничной разбивки

--- blog/views.py
+++ blog/views.py
@@ -1,13 +1,16 @@
 from .models import Post
 from django.shortcuts import render, get_object_or_404
+from django.views.generic import ListView
 
 
-def post_list(request):
-    posts = Post.published.all()
-    return render(request,
-                  'blog/post/list.html',
-                  {'posts': posts})
-
+class PostListView(ListView):
+    """
+    Альтернативное представление списка постов
+    """
+    queryset = Post.published.all()
+    context_object_name = 'posts'
+    paginate_by = 3
+    template_name = 'blog/post/list.html'
 
 def post_detail(request, year, month, day, post):
     post = get_object_or_404(Post,
